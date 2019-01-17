052625aba926b7306615cb2dd51831aa9a5187df
Sam Morrison <schmorrison@gmail.com>
2019-01-16 20:19:52 -0600

    install start project

---
+++ start_project.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install material
+ng add @angular/material
+# npm install --save @angular/material @angular/cdk @angular/animations
+# npm install --save hammerjs
+
+# Install Flexbox
+npm install @angular/flex-layout @angular/cdk --save
+
+# Install Firebase/Angularfire
+npm install firebase @angular/fire --save
+
+# Install helpers
+npm install ngx-pipes --save
